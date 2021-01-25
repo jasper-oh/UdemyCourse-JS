2e5dcfdc8acb837d951210984cef94eecb6a1e3c
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-01-25 18:03:32 +0900

    udemy js course-drum challenge

---
+++ udemy-course-js-drum-website/index.js
@@ -0,0 +1,47 @@
+const numberOfButton = document.querySelectorAll(".drum");
+for (i = 0; i < numberOfButton.length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    const buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+  });
+}
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      const audioCrash = new Audio("sounds/crash.mp3");
+      audioCrash.play();
+      break;
+    case "a":
+      const audioKick = new Audio("sounds/kick-bass.mp3");
+      audioKick.play();
+      break;
+    case "s":
+      const audioSnare = new Audio("sounds/snare.mp3");
+      audioSnare.play();
+      break;
+    case "d":
+      const audiotom1 = new Audio("sounds/tom-1.mp3");
+      audiotom1.play();
+      break;
+    case "j":
+      const audiotom2 = new Audio("sounds/tom-2.mp3");
+      audiotom2.play();
+      break;
+    case "k":
+      const audiotom3 = new Audio("sounds/tom-3.mp3");
+      audiotom3.play();
+      break;
+    case "l":
+      const audiotom4 = new Audio("sounds/tom-4.mp3");
+      audiotom4.play();
+      break;
+
+    default:
+      console.log();
+      break;
+  }
+}
